fa6cf799584ed38936bd21de5889f99e16d5cf3d
Kartikeya Sharma <workwithkartikeyasharma@gmail.com>
2026-03-09 12:43:33 +0530

    changes

---
+++ notification/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello_api(request):
+    return Response({
+        "message": "Hello from API"
+    })
